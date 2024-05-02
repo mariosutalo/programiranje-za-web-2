e708d6542dff4d29a97a863922537c2700cdb7b6
Mario Sutalo <mario.sutalo@gmail.com>
2024-05-02 10:15:37 +0200

    -cp

--- 10-dom-css/filter-a/app.js
+++ 10-dom-css/filter-a/app.js
@@ -1,35 +1,8 @@
-let goodyearClicked = false
-function onGoodyearClicked() {
-    let btn = document.querySelector('.filter-btn-1')
-    if (goodyearClicked === false) {
+function onTyreBrandClicked(brandName) {
+    let btn = document.querySelector(`.${brandName}`)
+    if (btn.classList.contains('filter-btn-selected') === false) {
         btn.classList.add('filter-btn-selected')
-        goodyearClicked = true
     } else {
         btn.classList.remove('filter-btn-selected')
-        goodyearClicked = false
-    }
-}
-
-let michelinClicked = false
-function onMichelinClicked() {
-    let btn = document.querySelector('.filter-btn-2')
-    if (michelinClicked === false) {
-        btn.classList.add('filter-btn-selected')
-        michelinClicked = true
-    } else {
-        btn.classList.remove('filter-btn-selected')
-        michelinClicked = false
-    }
-}
-
-let falkenClicked = false
-function onFalkenClicked() {
-    let btn = document.querySelector('.filter-btn-3')
-    if (falkenClicked === false) {
-        btn.classList.add('filter-btn-selected')
-        falkenClicked = true
-    } else {
-        btn.classList.remove('filter-btn-selected')
-        falkenClicked = false
     }
 }
