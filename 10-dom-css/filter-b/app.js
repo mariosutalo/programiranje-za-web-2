2527a8a469c5db9ce879013080a5cab07d080d6d
Mario Sutalo <mario.sutalo@gmail.com>
2024-05-10 08:15:21 +0200

    -cp

--- 10-dom-css/filter-b/app.js
+++ 10-dom-css/filter-b/app.js
@@ -1,35 +1,26 @@
-let iphoneClicked = false
 function onIphoneClicked() {
     const btn = document.querySelector('.iphone-btn')
-    if (iphoneClicked === false) {
+    if (btn.classList.contains('filter-btn-clicked') === false ) {
         btn.classList.add('filter-btn-clicked')
-        iphoneClicked = true
     } else {
         btn.classList.remove('filter-btn-clicked')
-        iphoneClicked = false
     }
 }
 
-let samsungClicked = false
 function onSamsungClicked() {
     const btn = document.querySelector('.samsung-btn')
-    if (samsungClicked === false) {
+    if (btn.classList.contains('filter-btn-clicked') === false) {
         btn.classList.add('filter-btn-clicked')
-        samsungClicked = true
     } else {
         btn.classList.remove('filter-btn-clicked')
-        samsungClicked = false
     }
 }
 
-let xiaomiClicked = false
 function onXiaomiClicked() {
     const btn = document.querySelector('.xiaomi-btn')
-    if (xiaomiClicked === false) {
+    if (btn.classList.contains('filter-btn-clicked') === false) {
         btn.classList.add('filter-btn-clicked')
-        xiaomiClicked = true
     } else {
         btn.classList.remove('filter-btn-clicked')
-        xiaomiClicked = false
     }
 }
